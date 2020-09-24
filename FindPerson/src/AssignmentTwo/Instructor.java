f0ad4a21af4b243123e050666de959a187caa39f
dopittman <dpittmancontact@gmail.com>
2020-09-24 18:49:49 -0400

    Create FindPerson Project
    Initialize new Java project building on to the FindInstructor assignment

---
+++ FindPerson/src/AssignmentTwo/Instructor.java
@@ -0,0 +1,4 @@
+package AssignmentTwo;
+
+public class Instructor {
+}
---
+++ FindPerson/src/AssignmentTwo/Main.java
@@ -0,0 +1,8 @@
+package AssignmentTwo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
