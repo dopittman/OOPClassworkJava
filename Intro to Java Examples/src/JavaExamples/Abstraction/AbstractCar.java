32064c3699801353bffd5789a404aa37c1ca8839
dopittman <dpittmancontact@gmail.com>
2020-10-11 14:33:23 -0400

    Complete Abstract Classs and Method example
    Write an abstract class, an extended class from the abstract class, and use abstract method.

--- Intro to Java Examples/src/JavaExamples/Abstraction/AbstractCar.java
+++ Intro to Java Examples/src/JavaExamples/Abstraction/AbstractCar.java
@@ -1,12 +1,16 @@
 package JavaExamples.Abstraction;
 
+  /* Abstract classes are not available to create objects directly.
+  *  Instead another class must inherit/extend from from them in order to create a new object from
+  *    the abstract class.
+  **/
 
-abstract class AbstractCar {
+  abstract class AbstractCar {
     //  Variables
-    private int wheels = 4;
-    private int vehicleSpeed = 0;
-    private String vehicleMake = "";
-    private boolean vehicleTurnedOn = false;
+    public int wheels = 4;
+    public int vehicleSpeed = 0;
+    public String vehicleModel = "";
+    public boolean vehicleTurnedOn = false;
 
     //  Abstract class to increase speed
     abstract void speedUp();
@@ -15,7 +19,7 @@ abstract class AbstractCar {
     abstract void slowDown();
 
     //  Turn the car on
-    public void turnCarOn() {
+    public void turnVehicleOn() {
         if (vehicleTurnedOn == true) {
             System.out.println("The vehicle is already turned on");
         } else if (vehicleTurnedOn == false) {
@@ -25,7 +29,7 @@ abstract class AbstractCar {
     }
 
     //  Turn the vehicle off
-    public void turnCarOff(){
+    public void turnVehicleOff(){
         if (vehicleTurnedOn == false) {
             System.out.println("The vehicle is already turned off");
         } else if (vehicleTurnedOn == true) {
@@ -34,7 +38,57 @@ abstract class AbstractCar {
         }
     }
 
+    //  Set vehicle make
+        public void setVehicleModel(String newModel){
+            this.vehicleModel = newModel;
+            System.out.println("You are driving a Ford " + this.vehicleModel);
+        }
+
+    //  Print out the current speed
+        public void currentSpeed(){
+            System.out.println("The current speed is:  " + this.vehicleSpeed);
+        }
+
     }
 
 
+    //  An example of a class extended from an Abstract class
+    class fordTruck extends AbstractCar {
 
+      //  Will speed up by 10 units when called
+      public void speedUp(){
+          if(this.vehicleSpeed <= 115){
+          this.vehicleSpeed += 10;
+          } else {
+              System.out.println("You are at the maximum speed!");
+          }
+      }
+
+      //  Will slow down by 10 units when called
+      public void slowDown() {
+          if(this.vehicleSpeed >= 10){
+              this.vehicleSpeed -= 10;
+          } else if(vehicleSpeed < 10 ){
+              System.out.println("You can not slow down anymore!");
+          }
+      }
+
+  }
+
+  class vehicleCreation {
+      public static void main(String[] args) {
+          fordTruck F150 = new fordTruck();
+
+          F150.setVehicleModel("F150");
+          F150.turnVehicleOn();
+          F150.speedUp();
+          F150.speedUp();
+          F150.speedUp();
+          F150.currentSpeed();
+          F150.slowDown();
+          F150.slowDown();
+          F150.slowDown();
+          F150.slowDown();
+          F150.turnVehicleOff();
+      }
+  }
