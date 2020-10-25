db1924009935038fb431267446b6f5603e3fb751
dopittman <dpittmancontact@gmail.com>
2020-10-25 15:11:45 -0400

    Add overload examples
    Created an exmaple of multiple types of overload in java

--- Intro to Java Examples/src/JavaExamples/Polymorphism/Overloading.java
+++ Intro to Java Examples/src/JavaExamples/Polymorphism/Overloading.java
@@ -1,4 +1,34 @@
 package JavaExamples.Polymorphism;
 
 public class Overloading {
+
+
+    int multiplyNums(int one, int two){
+        return one * two;
+    }
+
+    //  Overload with additional parameters
+    int multiplyNums(int one, int two, int three){
+        return one* two* three;
+    }
+
+    //  Overload with different types
+    int multiplyNums(int one, float two){
+        //  Convert to return an int
+        return Math.round(one * two);
+    }
+
+    //  Overload with types switched
+    int multiplyNums(float one, int two){
+        //  Convert to return an int
+        return Math.round(two * one);
+    }
+
+    public void main(String[] args) {
+        System.out.println(multiplyNums(2,3));
+        System.out.println(multiplyNums(2,3,4));
+        System.out.println(multiplyNums(1,  2.3455f));
+        System.out.println(multiplyNums(2.55f, 5));
+    }
 }
+
